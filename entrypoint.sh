ebc9bad1c52aa277ceab6c25da7629676a164733
Wesley Pettit <wppttt@amazon.com>
2019-11-21 23:39:17 -0800

    Print version on startup

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+echo -n "AWS for Fluent Bit Container Image Version "
+cat /AWS_FOR_FLUENT_BIT_VERSION
+/fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
