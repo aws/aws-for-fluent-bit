7bc12f72651c3749a95db9541fbf50a9881c5c08
Wesley Pettit <wppttt@amazon.com>
2019-12-06 16:29:13 -0800

    Bug fix: container exits with code 0 when sent a SIGTERM

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ tput bold;
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 tput sgr0;
-/fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
+exec -c /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
