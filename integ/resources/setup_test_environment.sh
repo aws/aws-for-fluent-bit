a035378420bd725d09608b7bb5ed4dbd265d5dec
Wesley Pettit <wppttt@amazon.com>
2020-03-02 18:45:33 -0800

    Remove echo from setup_test_environment.sh

--- integ/resources/setup_test_environment.sh
+++ integ/resources/setup_test_environment.sh
@@ -3,9 +3,6 @@
 # Using CloudFormation describe-stacks extracts the output values for kinesis stream and s3 bucket name, and sets them as environment variables
 stackOutputs=$(aws cloudformation describe-stacks --stack-name integ-test-fluent-bit --output text --query 'Stacks[0].Outputs[*].OutputValue')
 read -r -a outputArray <<< "$stackOutputs"
-echo "Kinesis Stream: ${outputArray[0]}"
-echo "S3 Bucket Name: ${outputArray[1]}"
-echo "Firehose Stream: ${outputArray[2]}"
 export FIREHOSE_STREAM="${outputArray[0]}"
 export KINESIS_STREAM="${outputArray[1]}"
 export S3_BUCKET_NAME="${outputArray[2]}"
