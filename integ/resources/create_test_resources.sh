5da9c8f34a398fcf28c1bc248ac4fc43e49961b2
Meghna Prabhu <meghnapr@amazon.com>
2020-08-28 10:44:47 -0700

    Support for ARM64 build

--- integ/resources/create_test_resources.sh
+++ integ/resources/create_test_resources.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # Deploys the CloudFormation template to create the stack and necessary resources- kinesis data stream, s3 bucket, and kinesis firehose delivery stream
-# Resource (stream, s3, delivery stream) names will start with the stack name "integ-test-fluent-bit"
-aws cloudformation deploy --template-file ./integ/resources/cfn-kinesis-s3-firehose.yml --stack-name integ-test-fluent-bit --region us-west-2 --capabilities CAPABILITY_NAMED_IAM
+# Resource (stream, s3, delivery stream) names will start with the stack name followed by the corresponding architecture. "integ-test-fluent-bit-architecture"
+ARCHITECTURE=$(uname -m)
+if [ ARCHITECTURE=="x86_64" ] 
+then
+    ARCHITECTURE="x86-64"
+fi
+aws cloudformation deploy --template-file ./integ/resources/cfn-kinesis-s3-firehose.yml --stack-name integ-test-fluent-bit-${ARCHITECTURE} --region us-west-2 --capabilities CAPABILITY_NAMED_IAM
