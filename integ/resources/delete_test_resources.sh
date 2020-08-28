75dedcaeb783dc111d4739bc79eddfda4debef1e
Meghna Prabhu <meghnapr@amazon.com>
2020-08-28 10:44:47 -0700

    Support for ARM64 build

--- integ/resources/delete_test_resources.sh
+++ integ/resources/delete_test_resources.sh
@@ -1,2 +1,3 @@
 # Delete the CloudFormation stack which created all the resources for running the integration test
-aws cloudformation delete-stack --stack-name integ-test-fluent-bit
+ARCHITECTURE=$(uname -m | tr '_' '-')
+aws cloudformation delete-stack --stack-name integ-test-fluent-bit-${ARCHITECTURE}
