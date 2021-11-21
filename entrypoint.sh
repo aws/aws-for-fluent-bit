1ebac568056a69d599975edb480b4edda04d1dd0
Pham, Thomas <Thomas.Pham@sicpa.com>
2021-11-21 13:58:46 +0100

    add FLUENT_BIT_CONFIG_FILE env variable to allow specify custom config path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
-exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
+
+FLUENT_BIT_CONFIG_FILE=${FLUENT_BIT_CONFIG_FILE:-'/fluent-bit/etc/fluent-bit.conf'}
+
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c ${FLUENT_BIT_CONFIG_FILE}
