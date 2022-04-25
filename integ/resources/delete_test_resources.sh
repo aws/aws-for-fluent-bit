b1f8cd74ca07adb8f17c8bf8d44304c088bc41cd
Wesley Pettit <wppttt@amazon.com>
2022-04-25 13:13:57 -0700

    integ: fix arm64 build
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- integ/resources/delete_test_resources.sh
+++ integ/resources/delete_test_resources.sh
@@ -1,3 +1,8 @@
 # Delete the CloudFormation stack which created all the resources for running the integration test
 ARCHITECTURE=$(uname -m | tr '_' '-')
+# For arm, uname evaluates to 'aarch64' but everywhere else in the pipline
+# we use 'arm64'
+if [ "$ARCHITECTURE" = "aarch64" ]; then
+    ARCHITECTURE="arm64"
+fi
 aws cloudformation delete-stack --stack-name integ-test-fluent-bit-${ARCHITECTURE}
