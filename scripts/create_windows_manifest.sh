8b025b962d147d12110bc54588315346e2da0f95
Harsh Rawat <rawahars@amazon.com>
2022-10-24 16:47:55 +0530

    updated the create manifest script to support multiple registries
    Presently, we are publishing the manifest of images to regional ECR, Public ECR, and dockerhub. Additionally, we also have a workflow to sync stable image in each region, wherein we create the manifest for the stable image.
    
    Therefore, in this change, we are-
    - Creating a common script which has common methods across various Windows scripts which we would be using in the release infrastructure.
    - Refactor the existing create manifest script to use multiple registries.
    - Changed the CMD in Windows dockerfile to exec format.
    - Created a new file for Windows stable version.
    - Updated versions in windows.version

--- scripts/create_windows_manifest.sh
+++ scripts/create_windows_manifest.sh
@@ -12,47 +12,148 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-set -euo pipefail
+set -uo pipefail
 
+#############################################################################################################
+# Start of the script
+#############################################################################################################
+
+# Source the common methods
+source ./common_windows.sh
+# Get the regional endpoint variable
+image_endpoint=$(get_regional_image_endpoint "${AWS_REGION}")
+
+# Declare constants
 export DOCKER_CLI_EXPERIMENTAL=enabled
 declare -A os_releases=( ["windows2019"]="ltsc2019" ["windows2022"]="ltsc2022")
 
-# Push the image to ECR with corresponding architecture as the tag.
-aws ecr get-login-password --region ${AWS_REGION}| docker login --username AWS --password-stdin ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+#############################################################################################################
+# Login into regional ECR of the region where we want to push the manifest.
+# We also generate the Repository path here specific to regional ECR.
+#############################################################################################################
+if [[ $REGISTRY_TO_PUSH == "private-ecr" ]]; then
+  # Create Repository path specific for private ECR.
+  REGISTRY="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.${image_endpoint}"
+  REPOSITORY_PATH="${REGISTRY}/${REPOSITORY}"
 
-# Find the applicable windows versions.
-cd $CODEBUILD_SRC_DIR
-versions=$(cat windows.versions | jq -c -r '.windows[].version')
-latest_version=""
+  # Login into ECR of the region.
+  aws ecr get-login-password --region ${AWS_REGION}| docker login --username AWS --password-stdin ${REGISTRY}
 
-# Create and push the image manifests.
-while read -r version; do
-  if [[ "$latest_version" < "$version" ]]; then
-    latest_version=$version
+#############################################################################################################
+# Login into public ECR of the account where we want to push the manifest.
+# In order to do so, we assume a cross-account IAM role.
+# We also generate the Repository path here specific to public ECR.
+#############################################################################################################
+elif [[ $REGISTRY_TO_PUSH == "public-ecr" ]]; then
+  # Create Repository path specific for public ECR.
+  REGISTRY="public.ecr.aws/${PUBLIC_REGISTRY_ALIAS}"
+  REPOSITORY_PATH="${REGISTRY}/${REPOSITORY}"
+
+  # Assume the publish role for public ecr.
+  CREDS=`aws sts assume-role --role-arn ${PUBLISH_ROLE_ARN_PUBLIC_ECR} --role-session-name publicECR`
+  export AWS_ACCESS_KEY_ID=`echo $CREDS | jq -r .Credentials.AccessKeyId`
+  export AWS_SECRET_ACCESS_KEY=`echo $CREDS | jq -r .Credentials.SecretAccessKey`
+  export AWS_SESSION_TOKEN=`echo $CREDS | jq -r .Credentials.SessionToken`
+
+  # Login into public ECR
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REGISTRY}
+
+#############################################################################################################
+# Login into dockerhub account where we want to push the manifest.
+# In order to do so, we get the credentials stored in Secrets Manager.
+# We also generate the Repository path here specific to dockerhub.
+#############################################################################################################
+elif [[ $REGISTRY_TO_PUSH == "dockerhub" ]]; then
+  # Create Repository path specific for Dockerhub.
+  REPOSITORY_PATH="${REPOSITORY}"
+
+  # Login into dockerhub account
+  DOCKER_HUB_SECRET="com.amazonaws.dockerhub.aws-for-fluent-bit.credentials"
+  docker login \
+  -u "$(aws secretsmanager get-secret-value --secret-id $DOCKER_HUB_SECRET --region us-west-2 | jq -r '.SecretString | fromjson.username')" \
+  --password "$(aws secretsmanager get-secret-value --secret-id $DOCKER_HUB_SECRET --region us-west-2 | jq -r '.SecretString | fromjson.password')"
+
+#############################################################################################################
+# Other registries are not supported.
+#############################################################################################################
+else
+  echo "Unsupported registry: ${REGISTRY_TO_PUSH}"
+  exit 1
+fi
+
+#############################################################################################################
+# "create-manifest" action is used to create and push manifests for Windows in the target region and account.
+#############################################################################################################
+if [[ $ACTION == "create-manifest" ]]; then
+
+  # Create and push the image manifests for all the versions.
+  while read -r version; do
+    # Create image manifest.
+    docker manifest create ${REPOSITORY_PATH}:${version}-windowsservercore \
+    ${REPOSITORY_PATH}:${version}-${os_releases[windows2019]} \
+    ${REPOSITORY_PATH}:${version}-${os_releases[windows2022]}
+    if [ $? = 1 ]; then
+      echo "Failed to create manifest for ${REPOSITORY_PATH}:${version}-windowsservercore"
+      exit 1
+    fi
+
+    # Sanity check the manifest.
+    docker manifest inspect ${REPOSITORY_PATH}:${version}-windowsservercore
+
+    # Push manifest to ECR.
+    docker manifest push ${REPOSITORY_PATH}:${version}-windowsservercore
+    if [ $? = 1 ]; then
+      echo "Failed to push manifest ${REPOSITORY_PATH}:${version}-windowsservercore"
+      exit 1
+    fi
+  done <<< "$(echo "${ALL_AWS_FOR_FLUENT_BIT_VERSIONS}")"
+
+  # Create manifest for latest image.
+  docker manifest create ${REPOSITORY_PATH}:windowsservercore-latest \
+  ${REPOSITORY_PATH}:${AWS_FOR_FLUENT_BIT_LATEST_VERSION}-${os_releases[windows2019]} \
+  ${REPOSITORY_PATH}:${AWS_FOR_FLUENT_BIT_LATEST_VERSION}-${os_releases[windows2022]}
+  if [ $? = 1 ]; then
+    echo "Failed to create manifest for ${REPOSITORY_PATH}:windowsservercore-latest"
+    exit 1
   fi
 
-  docker manifest create ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${version}-windowsservercore \
-  ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${version}-${os_releases[windows2019]} \
-  ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${version}-${os_releases[windows2022]}
+  # Push manifest for latest image.
+  docker manifest push ${REPOSITORY_PATH}:windowsservercore-latest
+  if [ $? = 1 ]; then
+    echo "Failed to push manifest ${REPOSITORY_PATH}:windowsservercore-latest"
+    exit 1
+  fi
 
-  # Sanity check the manifest.
-  docker manifest inspect ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${version}-windowsservercore
+#############################################################################################################
+# "sync-stable-image" action is used to sync stable image in target repository/region/account.
+#############################################################################################################
+elif [[ $ACTION == "sync-stable-image" ]]; then
+  # Obtain the image URLs for expected and actual stable Windows image.
+  EXPECTED_IMAGE_FOR_STABLE="${REPOSITORY_PATH}:${AWS_FOR_FLUENT_BIT_STABLE_VERSION}-windowsservercore"
+  ACTUAL_IMAGE_FOR_STABLE="${REPOSITORY_PATH}:windowsservercore-stable"
 
-  # Push manifest to ECR.
-  docker manifest push ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${version}-windowsservercore
-done <<< "$(echo "$versions")"
+  # If the image manifests are not same then create and push the latest one.
+  compare_image_manifests "${EXPECTED_IMAGE_FOR_STABLE}" "${ACTUAL_IMAGE_FOR_STABLE}"
+  if [ $? = 1 ]; then
+      # Create manifest for stable image.
+      docker manifest create ${REPOSITORY_PATH}:windowsservercore-stable \
+      ${REPOSITORY_PATH}:${AWS_FOR_FLUENT_BIT_STABLE_VERSION}-${os_releases[windows2019]} \
+      ${REPOSITORY_PATH}:${AWS_FOR_FLUENT_BIT_STABLE_VERSION}-${os_releases[windows2022]}
 
-# Create and push manifest for latest image.
-docker manifest create ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:windowsservercore-latest \
-${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${latest_version}-${os_releases[windows2019]} \
-${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${latest_version}-${os_releases[windows2022]}
-
-docker manifest push ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:windowsservercore-latest
-
-# Create and push manifest for stable image.
-stable_version=$(cat AWS_FOR_FLUENT_BIT_STABLE_VERSION)
-docker manifest create ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:windowsservercore-stable \
-${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${stable_version}-${os_releases[windows2019]} \
-${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:${stable_version}-${os_releases[windows2022]}
-
-docker manifest push ${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY}:windowsservercore-stable
+      # Push manifest for stable image.
+      docker manifest push ${REPOSITORY_PATH}:windowsservercore-stable
+      if [ $? = 1 ]; then
+        echo "Failed to push manifest ${REPOSITORY_PATH}:windowsservercore-stable"
+        exit 1
+      fi
+      echo "Synced the latest version of stable image"
+  else
+    echo "Stable image is already in sync"
+  fi
+#############################################################################################################
+# Any other action is not supported.
+#############################################################################################################
+else
+  echo "Unsupported action: ${ACTION}"
+  exit 1
+fi
