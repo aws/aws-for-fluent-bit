834319f61203a634f6e233dcd882f77560a62b8b
Harsh Rawat <rawahars@amazon.com>
2022-11-12 16:14:37 +0530

    windows: add debugging option to the entrypoint script
    In order to obtain code dumps on Windows, we need to set a registry key which specifies the dump folder as well as the level of dump information.
    
    We will be adding a parameter in the entrypoint.ps1 script which the customers can enable to generate the core dumps. Since these core dump files would be generated inside the container, we need to bind mount the location to the host to retrieve the same.
    
    Signed-off-by: Harsh Rawat <rawahars@amazon.com>

--- scripts/entrypoint.ps1
+++ scripts/entrypoint.ps1
@@ -8,6 +8,10 @@
     .PARAMETER ConfigFile
     [Optional] Specifies the region. Defaults to 'C:\fluent-bit\etc\fluent-bit.conf'.
 
+    .PARAMETER EnableCoreDump
+    [Optional] Specifies if the core dump needs to be generated on Fluent Bit crash. Defaults to false.
+    The dump is generated at location 'C:\fluent-bit\CrashDumps' inside the container and needs to be bind-mounted to the host to retrieve the same.
+
     .INPUTS
     None. You cannot pipe objects to this script.
 
@@ -15,16 +19,29 @@
     None. This script does not generate an output object.
 
     .EXAMPLE
-    PS> .\entrypoint.ps1 -ConfigFile "C:\ecs_windows\cloudwatch.conf"
+    PS> .\entrypoint.ps1 -ConfigFile "C:\ecs_windows_forward_daemon\cloudwatch.conf"
     Runs the aws-for-fluent-bit Windows image with the CloudWatch config baked into the image.
 #>
 Param(
     [Parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    [string]$ConfigFile = "C:\fluent-bit\etc\fluent-bit.conf"
+    [string]$ConfigFile = "C:\fluent-bit\etc\fluent-bit.conf",
+
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [switch]$EnableCoreDump
 )
 
 $version = Get-Content -Path "C:\AWS_FOR_FLUENT_BIT_VERSION"
 Write-Host "AWS for Fluent Bit Container Image Version ${version}"
+
+if ($EnableCoreDump) {
+    Write-Host "Setting the registry keys to collect dumps"
+
+    # Setting registry keys based on https://learn.microsoft.com/en-us/windows/win32/wer/collecting-user-mode-dumps
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name "LocalDumps"
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" -Name "DumpFolder" -Value "C:\fluent-bit\CrashDumps" -PropertyType ExpandString
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" -Name "DumpType" -Value 2 -PropertyType DWord
+}
 
 C:\fluent-bit\bin\fluent-bit.exe -e C:\fluent-bit\kinesis.dll -e C:\fluent-bit\firehose.dll -e C:\fluent-bit\cloudwatch.dll -c "${ConfigFile}"
