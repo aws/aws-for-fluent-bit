4d7a4ef3407511ef2cc151c3454309756aef68d0
Wesley Pettit <wppttt@amazon.com>
2024-03-13 13:24:52 -0700

    scripts: fix athena usage query to support december
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- scripts/athena_usage_query/exec_query.sh
+++ scripts/athena_usage_query/exec_query.sh
@@ -7,8 +7,8 @@ FROM ${TABLE_NAME}
 WHERE
     eventsource = 'ecr.amazonaws.com' AND
     eventname in ('GetDownloadUrlForLayer', 'BatchGetImage') AND
-    eventTime > '${YEAR}-${START}-01T00:00:00Z' AND
-    eventTime < '${YEAR}-${END}-01T00:00:00Z'
+    eventTime > '${START_YEAR}-${START}-01T00:00:00Z' AND
+    eventTime < '${END_YEAR}-${END}-01T00:00:00Z'
 "
 }
 
@@ -19,19 +19,19 @@ get_version_download_query() {
 
 usage() {
     echo "Requires: AWS CLI, jq. Run with creds for the account."
-    echo "Usage: bash $0 {all | version number} {AWS Region} {AWS Account ID} {Year} {Target Month} {Next Month}"
+    echo "Usage: bash $0 {all | version number} {AWS Region} {AWS Account ID} {Start Year} {Target/first Month} {End Year} {Next/excluded Month}"
     echo "If you want November's downloads, then Target Month=11, Next Month=12"
     echo "Outputs the Athena Query Execution ID, which can be used with get_results.sh"
     echo "-----Examples: Query total image downloads for a month-----"
-    echo "CLASSIC: bash exec_query.sh all us-west-2 906394416424 2023 11 12"
-    echo "BAH: bash $0 all me-south-1 741863432321 2023 11 12"
-    echo "HKG: bash $0 all ap-east-1 449074385750 2023 11 12"
-    echo "MXP: bash $0 all eu-south-1 960320637246 2023 11 12"
-    echo "CPT: bash $0 all af-south-1 928143927712 2023 11 12"
-    echo "USGOV: bash $0 all us-gov-west-1 161423150738 2023 11 12"
+    echo "CLASSIC: bash exec_query.sh all us-west-2 906394416424 2023 11 2023 12"
+    echo "BAH: bash $0 all me-south-1 741863432321 2023 12 2024 01"
+    echo "HKG: bash $0 all ap-east-1 449074385750 2024 01 2024 02"
+    echo "MXP: bash $0 all eu-south-1 960320637246 2023 11 2023 12"
+    echo "CPT: bash $0 all af-south-1 928143927712 2023 01 2023 02"
+    echo "USGOV: bash $0 all us-gov-west-1 161423150738 2023 11 2023 12"
     echo "-----Examples: Query total image downloads for specific version in a month-----"
     echo "bash exec_query.sh 2.28.4 us-west-2 906394416424 2023 11 12"
-    echo "bash exec_query.sh 2.31.12.20230727 us-west-2 906394416424 2023 11 12"
+    echo "bash exec_query.sh 2.31.12.20230727 us-west-2 906394416424 2023 11 2023 12"
     exit 1;
 }
 
@@ -67,8 +67,8 @@ then
     usage
 fi
 
-export YEAR=$4
-if [ -z "${YEAR}" ];
+export START_YEAR=$4
+if [ -z "${START_YEAR}" ];
 then
     usage
 fi
@@ -79,7 +79,13 @@ then
     usage
 fi
 
-export END=$6
+export END_YEAR=$6
+if [ -z "${END_YEAR}" ];
+then
+    usage
+fi
+
+export END=$7
 if [ -z "${END}" ];
 then
     usage
@@ -103,6 +109,7 @@ fi
 
 if [ "${VERSION}" = "all" ]; then
     get_all_download_query
+    exec_query
 else
     get_version_download_query
     exec_query
