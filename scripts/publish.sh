25a70fb6c574cde6952ccdebdbe6b21ce3bb3724
Prateek Chaudhry <prateek.chaudhry@gmail.com>
2026-01-13 11:47:20 -0800

    Remove check_tag_exists guard from publish_ssm for BUILD_VERSION=3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -264,19 +264,6 @@ publish_to_public_ecr() {
 }
 
 publish_ssm() {
-	# Check if BUILD_VERSION=3 images exist before publishing SSM parameters
-	if [ "$BUILD_VERSION" = "3" ]; then
-		if ! check_tag_exists "${2}" "${3}"; then
-			echo "BUILD_VERSION=3 image ${2}:${3} not found, skipping SSM parameter publish for region ${1}"
-			return 0
-		fi
-
-		if ! check_tag_exists "${2}" "init-${3}"; then
-			echo "BUILD_VERSION=3 init image ${2}:init-${3} not found, skipping SSM parameter publish for region ${1}"
-			return 0
-		fi
-	fi
-
 	# This optional parameter indicates if we should publish stable (defaults to false)
 	if [ ${4:-false} = true ]; then
 		aws ssm put-parameter --name /aws/service/aws-for-fluent-bit/stable --overwrite \
